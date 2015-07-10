c602adc2fb312ac46be416c327c53ad21d6ab4e5
Grace O'Hair-Sherman <grace.ohair.sherman@gmail.com>
2015-07-10 16:38:39 -0700

    Initial Commit

---
+++ proteinApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  proteinApp
+//
+//  Created by Grace O'Hair-Sherman on 7/10/15.
+//  Copyright (c) 2015 Grace O'Hair-Sherman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
